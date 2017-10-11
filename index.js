d6eb7bfe8b1b47480cb83a1e1f1a947a8ca7a266
Adam <trzcinski.adam@gmail.com>
2017-10-11 20:38:17 +0200

    Copy images to ios path. Create JSON file

--- index.js
+++ index.js
@@ -3,55 +3,56 @@
 *
 * @param pathname Path to a folder containing images.
 */
+
 const path = require('path');
 const fs = require('fs');
-const chalk = require('chalk');
 
 const copyImages = require('./lib/copyImages').copyImages;
+const log = require('./lib/log').log;
 
 const exit = msg => {
-  console.log(chalk.red(msg));
+  log('red', msg);
   process.exit(1);
 };
 
-const makeIOsPath = projectName => {
+const getiosPath = projectName => {
   // TODO: optional arg 'projectName' = to be accessed instead of 1st element?
   const ios = path.join(__dirname, 'ios');
   const iosContent = fs.readdirSync(ios);
-  const iosPath = path.join(__dirname, iosContent[0], 'Images.xcassets');
+  const iosPath = path.join(ios, iosContent[0], 'Images.xcassets');
   return iosPath;
 };
 
-const makeImages = pathname => {
+const makeImages = (pathname, projectName) => {
   // console.log(process.argv);
+  // TODO: optional arg 'projectName' = to be accessed instead of 1st element?
   if (!pathname) exit(`Please provide path to your images folder. Exiting.`);
   const absPath = path.join(__dirname, pathname);
   const androidPath = path.join(
     __dirname,
     '/android/app/src/main/res/drawable'
   );
-  const iosPath = makeIOsPath();
-  console.log('iosPath: ', iosPath);
-  // access directory
+  const iosPath = getiosPath(projectName);
   fs.readdir(absPath, (err, files) => {
-    if (err) exit(`Could not acces pathname. ${err}`);
-    // filter all images
+    if (err) exit(`Could not access pathname. ${err}`);
     const images = files.filter(file =>
       path.extname(file).match(/.(jpg|jpeg|png|gif)$/i)
     );
     if (!images || !images.length) {
       exit('No images found. Aborting.');
     }
-    // check if 'drawable' dir exists, if not, create one
     fs.mkdir(androidPath, err => {
       if (!err || err.code === 'EEXIST')
         copyImages(images, absPath, androidPath);
       else
-        console.log(
-          chalk.red(`Error creating 'drawable' dir inside Android path. ${err}`)
-        );
+        log('red', `Error creating 'drawable' dir inside Android path. ${err}`);
     });
-    copyImages(images, absPath, iosPath, true);
+    fs.mkdir(iosPath, err => {
+      if (!err || err.code === 'EEXIST')
+        copyImages(images, absPath, iosPath, true);
+      else
+        log('red', `Error creating 'drawable' dir inside Android path. ${err}`);
+    });
   });
 };
 
