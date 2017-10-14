680a7523e40e8078179ffd5fbfba210ca7c87d4b
Adam <trzcinski.adam@gmail.com>
2017-10-14 12:29:10 +0200

    Added help command.

--- bin/images.js
+++ bin/images.js
@@ -1,15 +1,27 @@
 #!/usr/bin/env node
+/* eslint no-console: 0 */
+
 const fs = require('fs');
 const path = require('path');
 const makeImages = require('../index').makeImages;
 
-const printHelp = () => console.log('rn-images-asseets help: ...');
+const printHelp = () => {
+  const help = `
+    rn-images-asseets
+
+    Available commands: 
+      -h, -help     Display this help
+      -v, -version  Display version
+      ./xxx/xxx     Copy images from ./xxx/xxx path
+  `;
+  console.log(help);
+};
+
 const version = JSON.parse(
   fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8')
 ).version;
 
-const printVersion = () =>
-  console.log(`rn-images-asseets. Version: ${version}`);
+const printVersion = () => console.log(`Version: ${version}`);
 
 const images = arg => {
   switch (arg) {
--- test.js
+++
@@ -1,4 +0,0 @@
-const lib = require('./index');
-const makeImages = lib.makeImages;
-
-makeImages('./images');
